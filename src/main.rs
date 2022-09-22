0f30b57bc2539bb7b0ba1c00c786a76b9f2aee16
Edu <eduardvf@outlook.com>
2022-09-23 00:30:53 +0200

    prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+// main.rs
+
+use rustyline::{Editor, error::ReadlineError};
+
+struct Env;
+
+fn repl(env: &mut Env) -> Result<(), ReadlineError> {
+
+    let mut readline: Editor<()> = Editor::<()>::new()?;
+    loop {
+        let input = readline.readline("(lluna) ");
+        
+        match input {
+            Ok(line) => {
+                println!("{}", &line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                break;
+            }
+            Err(_) => {
+                break;
+            }
+        }
+    }
+    Ok(())
+}
+
 fn main() {
     println!("Hello, world!");
+    let mut env = Env;
+    repl(&mut env);
 }
